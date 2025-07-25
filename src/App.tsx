1859fd029a174ae80adca7e6e161fc8d251a70b6
Ali Sayyed <69006996+adsayyed@users.noreply.github.com>
2025-07-24 23:02:14 -0500

    Add inline time editing to time zone cards

--- src/App.tsx
+++ src/App.tsx
@@ -273,7 +273,7 @@ function App() {
                 onRemove={() => handleRemoveTimeZone(zone.id)}
                 theme={theme}
                 canRemove={timeZones.length > 1}
-                onTimeClick={handleTimeZoneTimeClick}
+                onTimeSubmit={handleTimeInput}
               />
             ))}
           </div>
