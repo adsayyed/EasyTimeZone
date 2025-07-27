670c19468d911ce7060c341c95c811c6c0efb31f
Ali Sayyed <69006996+adsayyed@users.noreply.github.com>
2025-07-26 21:47:26 -0500

    Restore original card dimensions

--- src/components/TimeZoneCard.tsx
+++ src/components/TimeZoneCard.tsx
@@ -91,7 +91,7 @@ const TimeZoneCard: React.FC<TimeZoneCardProps> = ({
 
   return (
     <div 
-      className={`${getCardClasses()} w-full max-w-xs lg:max-w-56`}
+      className={`${getCardClasses()}`}
       onClick={handleClick}
       role="button"
       tabIndex={0}
